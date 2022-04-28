db3c8b6d357555eeaedb0104d9d148ee10f75f01
Sven Vetter <git.09o@svenvetter.com>
2022-04-28 15:14:51 +0200

    initial release

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+
+
+## Version v1.0.0
+*Thu, 28 Apr 2022 13:14:51 +0000*
+- initial release
---
+++ bin/release.php
@@ -0,0 +1,25 @@
+#!/usr/bin/env php
+<?php
+
+$version = "v1.0.0";
+$message = "initial release";
+
+/* echo("Running phpstan:\n");
+system("composer run-script phpstan", $res);
+if ($res>0) {
+  echo("\nError during execution phpstan. Releasing cannceled.\n");
+  return 1;
+} */
+
+file_put_contents("CHANGELOG.md", "\n\n## Version " . $version, FILE_APPEND);
+file_put_contents("CHANGELOG.md", "\n*" . date("r") . "*", FILE_APPEND);
+file_put_contents("CHANGELOG.md", "\n- " . $message . "\n", FILE_APPEND);
+
+$res = shell_exec('git add .');
+$res = shell_exec('git commit -m "' . $message . '"');
+$res = shell_exec('git push');
+
+$res = shell_exec('git tag -a ' . $version . ' -m "' . $message . '"');
+$res = shell_exec('git push origin ' . $version);
+
+?>
